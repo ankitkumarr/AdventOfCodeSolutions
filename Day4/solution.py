c11401424c4da7945ac3c5c7749614667f534471
Ankit Kumar <ankitkumarr@users.noreply.github.com>
2016-02-17 03:42:12 -0500

    Adding Day 4

---
+++ Day4/solution.py
@@ -0,0 +1,19 @@
+import hashlib
+
+
+def hashfunction1(stri):
+	m = hashlib.md5()
+	m.update(stri)
+	return m.hexdigest()
+
+m = hashlib.md5()
+s = 'iwrupvqb'
+x = 0
+while (True):
+	st = s + str(x)
+	if hashfunction1(st)[:5] == '00000':
+		break
+	x+=1
+print "The answer is: ", x
+
+
